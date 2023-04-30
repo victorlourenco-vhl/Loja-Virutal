ae1c9e373d147f8741023816625ec0a58d9da83f
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-04-29 21:38:38 -0300

    Criando o back-end

---
+++ loja-virtual/src/test/java/com/victor/lojavirtual/LojaVirtualApplicationTests.java
@@ -0,0 +1,13 @@
+package com.victor.lojavirtual;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaVirtualApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
