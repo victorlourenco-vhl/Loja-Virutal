2525e31a5aeb7c1044350579a791000e613708c8
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-04-30 11:21:07 -0300

    FEAT: Adicionando o atribuito ID no estado, adiconando profile de tests e dev .properties

--- backend/src/main/java/com/victor/lojavirtual/domain/Estado.java
+++ backend/src/main/java/com/victor/lojavirtual/domain/Estado.java
@@ -4,12 +4,18 @@ import java.io.Serializable;
 import java.util.Date;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 
 @Entity
 public class Estado implements Serializable{
 
 	private static final long serialVersionUID = 1L;
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
 	private String name;
 	private String sigla;
 	private Date dataCriacao;
