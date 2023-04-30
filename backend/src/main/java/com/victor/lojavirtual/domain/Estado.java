18754aa6781d6f3fa94767b4dd63fd2bbd575a64
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-04-30 10:48:18 -0300

    FEAT: Implementando a classe Estado

---
+++ backend/src/main/java/com/victor/lojavirtual/domain/Estado.java
@@ -0,0 +1,61 @@
+package com.victor.lojavirtual.domain;
+
+import java.io.Serializable;
+import java.util.Date;
+
+import jakarta.persistence.Entity;
+
+@Entity
+public class Estado implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+	private String name;
+	private String sigla;
+	private Date dataCriacao;
+	private Date dataAtualizacao;
+		
+	public Estado() {
+		
+	}
+
+	public Estado(String name, String sigla, Date dataCriacao, Date dataAtualizacao) {
+		super();
+		this.name = name;
+		this.sigla = sigla;
+		this.dataCriacao = dataCriacao;
+		this.dataAtualizacao = dataAtualizacao;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getSigla() {
+		return sigla;
+	}
+
+	public void setSigla(String sigla) {
+		this.sigla = sigla;
+	}
+
+	public Date getDataCriacao() {
+		return dataCriacao;
+	}
+
+	public void setDataCriacao(Date dataCriacao) {
+		this.dataCriacao = dataCriacao;
+	}
+
+	public Date getDataAtualizacao() {
+		return dataAtualizacao;
+	}
+
+	public void setDataAtualizacao(Date dataAtualizacao) {
+		this.dataAtualizacao = dataAtualizacao;
+	}
+}
