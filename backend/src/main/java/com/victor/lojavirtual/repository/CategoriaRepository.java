abf2d3272dcf9e0f704fcf4a85296df672a81d4a
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-05-08 22:25:43 -0300

    FEAT: Adiconando ID na categoria e implementado CategoriaService

--- backend/src/main/java/com/victor/lojavirtual/repository/CategoriaRepository.java
+++ backend/src/main/java/com/victor/lojavirtual/repository/CategoriaRepository.java
@@ -1,5 +1,9 @@
 package com.victor.lojavirtual.repository;
 
-public interface CategoriaRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.victor.lojavirtual.domain.Categoria;
+
+public interface CategoriaRepository extends JpaRepository<Categoria, Integer>{
 
 }
