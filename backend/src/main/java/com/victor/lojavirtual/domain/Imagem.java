b00944a44cab51f5fb2ef40e0a9451e906cf72fc
Victor Lourenço <victor.lourenco@tgid.com.br>
2023-05-22 08:55:44 -0300

    FEAT: Fazendo o relacionamento de Produto e Imagem

--- backend/src/main/java/com/victor/lojavirtual/domain/Imagem.java
+++ backend/src/main/java/com/victor/lojavirtual/domain/Imagem.java
@@ -8,7 +8,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
 import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -27,6 +29,10 @@ public class Imagem implements Serializable {
 
 	@Lob
 	private byte[] imageData;
+	
+	@ManyToOne
+	@JoinColumn(name = "produto_id")
+	private Produto produto;
 
 	public Imagem() {
 	}
